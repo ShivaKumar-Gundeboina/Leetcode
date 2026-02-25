c474e482359d3c39e62a034fcbe6d18c92292685
ShivaKumar <95913034+ShivaKumar-Gundeboina@users.noreply.github.com>
2026-02-25 16:43:00 +0530

    Time: 7 ms (56.46%) | Memory: 57 MB (82.38%) - LeetSync

---
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    let longestString=0;
+    let set=new Set()
+
+    let left=0;
+    let right=0;
+
+    while (right<s.length){
+        let letter=s[right];
+       
+         
+        if(!set.has(letter)){
+            set.add(letter)
+            longestString=Math.max(longestString,set.size)
+            right++
+        }else{
+            set.delete(s[left]);
+            left++
+        }
+       
+    }
+    return longestString
+
+};
